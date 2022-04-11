6c8ae868c7eb82d3487179cb491901a4d7d32f14
Nicolas <nicolas.charles@epita.fr>
2022-04-11 17:20:43 +0200

    remove data

--- data.py
+++
@@ -1,8 +0,0 @@
-token = 'Nzk5MzgxMTIxMzAwNTYxOTgx.YACvrw.UBsj243x1p5DJ_27QXwEshBjLC4'
-my_api_key = '259d627dd9e34681a7111283a8f244ef'
-root = "https://www.bungie.net/Platform/Destiny2/"
-icon_root = "https://www.bungie.net"
-characters_ids = ['2305843009410737586', '2305843009699615564', '2305843009750924016']
-membership_ids = ['4611686018484966940', '4611686018522011827']
-membership_types = [3, 2]
-auth_token = ''
